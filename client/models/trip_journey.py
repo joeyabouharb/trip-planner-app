30733d2bcf217156975c7abe9572b59648b67524
joeyabouharb <joeyabouharb@gmail.com>
2019-09-28 15:18:59 +1000

    refactored code in data factory and created trip journey model to store data

---
+++ client/models/trip_journey.py
@@ -0,0 +1,54 @@
+"""
+Trip Journey Class Model Object
+"""
+
+
+class TripJourney():
+    """
+    model class defining properties for a single journey,
+    including total fares, duration, stop info and departure/ arrival times
+    """
+    def __init__(self, *args):
+
+        total_fare, total_duration, summary,\
+        depart_day, depart_time,\
+        arrive_day, arrive_time, stops = args
+
+        self.total_fare = total_fare
+        self.total_duration = total_duration
+        self.summary = summary
+        self.departure = depart_day, depart_time
+        self.arrival = arrive_day, arrive_time
+        self.stops = stops
+
+
+    def to_str(self):
+        """
+        return a meaningful string message containing information
+        about the particular journey to serve to the client
+        """
+        pass
+        # for trip_info, stop_data in stops.items():
+        #     trip_str += f"\n\n{trip_info}\n"
+        #     trip_str += "\n".join(stop_data)
+        # print(
+        #     f"{depart_day} - {depart_time}" ' to ' f"{arrive_day} - {arrive_time}\n"
+        #     f' Trip Duration: {total_duration} minutes Cost: {total_fare}\n'
+        #     "transport used: ",
+        #     " -> ".join(summary), '\n',
+        #     trip_str
+        # )
+
+
+    def to_dict(self):
+        """
+        turn to a class object to a dictionary
+        """
+        pass
+
+
+    def number_of_stops(self):
+        """
+        get the total number of stops / trip changes in a journey
+        """
+        pass
--- client/services/data_factory.py
+++ client/services/data_factory.py
@@ -5,8 +5,7 @@ filtering and using algorithms
 from datetime import datetime
 from dateutil import tz
 from swagger_client.models import (
-    DepartureMonitorResponse, StopFinderResponse,
-    StopFinderLocation, TripRequestResponseJourney
+    DepartureMonitorResponse
 )
 from client.services.app_locals import VALID_TRANSPORT
 
@@ -19,7 +18,7 @@ def date_parser(departure_time, time_format="%Y-%m-%dT%H:%M:%SZ"):
     # Specify Timezone to convert to and from ie UTC -> Sydney localtime
     from_zone = tz.gettz('UTC')
     to_zone = tz.gettz('Australia/Sydney')
-    
+
     # replace will specify the date format to convert from
     # and astimezone will convert it to Australian GMT
     parsed_date = datetime.strptime(
@@ -31,7 +30,7 @@ def date_parser(departure_time, time_format="%Y-%m-%dT%H:%M:%SZ"):
 
 
 def generator_departure_info(
-    events: DepartureMonitorResponse
+        events: DepartureMonitorResponse
 ) -> (int, int, int, str, str):
     """
     yields departure information as
@@ -45,7 +44,7 @@ def generator_departure_info(
         transportation = event.transportation
         route = transportation.number
         dest = transportation.destination.name
-        location = event.location
+        # location = event.location
 
         departure_time = event.departure_time_planned
         parsed_date = date_parser(departure_time)
@@ -58,15 +57,18 @@ def generator_departure_info(
         # in order to calculate the hours, mins and secs, we must
         # divide the total seconds to produce total hours and divide the
         # remainder to find minutes and seconds
-        hours, remainder = divmod(countdown.seconds, 3600) # will return division result + remainder
-        minutes, seconds = divmod(remainder, 60) # devide remainder by 60 remainder which will be seconds
+
+        # will return division result + remainder
+        hours, remainder = divmod(countdown.seconds, 3600)
+        # devide remainder by 60 remainder which will be seconds
+        minutes, seconds = divmod(remainder, 60)
         yield (
-                hours, minutes, seconds, route, dest
+            hours, minutes, seconds, route, dest
         )
 
 
 def create_date_and_time(
-    date: datetime, format_date: str, format_time: str
+        date: datetime, format_date: str, format_time: str
 ):
     """
     returns a tuple of strings containing a formatted
@@ -87,39 +89,21 @@ def generator_trip_data(journeys):
     """
     create trip date. TBD
     """
-    for journey in journeys:
-        legs = journey.legs
-        fares = journey.fare.tickets
-        total_fare = 0
 
-        for fare in fares:
-            if fare.person == 'SCHOLAR':
-                total_fare += float(fare.properties.price_total_fare)
-
-        total_duration = 0
-        summary = []
-        leg_number = 0
-        names = []
-        types = []
-        for leg_number, leg in enumerate(legs):
-            total_duration += leg.duration
-            origin = leg.origin
-            destination = leg.destination
-
-            # departure time
-            if leg_number == 0:
-                depart = date_parser(origin.departure_time_estimated)
-                depart_day, depart_time = create_date_and_time(depart, '%A,  %d-%m-%Y', '%H:%M%Z')
-            # arrival time
-            if leg_number == len(legs) - 1:
-                arrive = date_parser(destination.arrival_time_estimated)
-                arrive_day, arrive_time = create_date_and_time(arrive, '%A,  %d-%m-%Y', '%H:%M%Z')
-
+    def get_stop_info(stops, legs):
+        """
+        """
+        for leg in legs:
             for seq_num, sequence in enumerate(leg.stop_sequence):
-                # attempt to get live updates/ est. otherwise get planned dep/arrival times
                 if seq_num == 0:
-                    named = leg.transportation.name if leg.transportation.name is not None else 'walk'
-                    names.append(named)
+                    # create new key entry for each leg
+                    type_ = (
+                        leg.transportation.name
+                        if leg.transportation.name is not None else 'walk'
+                    )
+                    stops[type_] = []
+
+                # attempt to get live updates/ est. otherwise get planned dep/arrival times
                 if sequence.departure_time_estimated is not None:
                     departure = date_parser(sequence.departure_time_estimated)
                 elif sequence.arrival_time_estimated is not None:
@@ -134,28 +118,36 @@ def generator_trip_data(journeys):
                 if departure != 'Unavailable':
                     # parse dates and return the formatted time string
                     _, departure = create_date_and_time(departure, '', '%H:%M')
-                names.append(f'{sequence.name} arrives: {departure}')
-                if seq_num == len(leg.stop_sequence) - 1:
-                    names.append('Alight Here')
+                stops[type_].append(f'{sequence.name} arrives: {departure}')
 
-            # get transport type for each trip segment/ leg
-            transportation = leg.transportation
-            
-            # append trip type ie. Trains or light rail to summary by searching
-            # dict of valid transports
-            summary.append(VALID_TRANSPORT[transportation.product._class])
+    for journey in journeys:
 
+        legs = journey.legs
+        fares = journey.fare.tickets
 
-        # total minutes
-        minutes = total_duration / 60
-        total_fare = round(total_fare, 2)
-        # return meaningful data collected
-        print(
-            f'Total Stop Changes: {leg_number + 1}\n'
-            f"{depart_day} - {depart_time}" ' to ' f"{arrive_day} - {arrive_time}\n"
-            f' Trip Duration: {minutes} minutes Cost: {total_fare}\n'
-            "transport used: ",
-            " -> ".join(summary), '\n',
-            '\n'.join(names)
+        total_fare = sum(
+            float(fare.properties.price_total_fare)
+            for fare in fares if fare.person == 'SCHOLAR'
         )
-        input()
+        total_duration = sum(leg.duration for leg in legs) / 60
+        total_duration = round(total_duration, 2)
+
+        summary = [
+            VALID_TRANSPORT[leg.transportation.product.icon_id]
+            for leg in legs
+        ]
+
+        depart = date_parser(legs[0].origin.departure_time_estimated)
+        depart_day, depart_time = create_date_and_time(depart, '%A,  %d-%m-%Y', '%H:%M%Z')
+
+        arrive = date_parser(legs[-1].destination.arrival_time_estimated)
+        arrive_day, arrive_time = create_date_and_time(arrive, '%A,  %d-%m-%Y', '%H:%M%Z')
+
+        stops = {}
+        get_stop_info(stops, legs)
+
+        yield (
+            total_fare, total_duration, summary, depart_day, depart_time,
+            arrive_day, arrive_time, stops
+        )
+
