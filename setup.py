249252a00a567726accde7b6157a47181efde9c6
joeyabouharb <joeyabouharb@gmail.com>
2019-10-04 12:59:25 +1000

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages 
+from setuptools import setup, find_packages
 
 NAME = "trip_planner"
 VERSION = "1.0.0"
@@ -6,30 +6,30 @@ DESCRIPTION = "Trip Planner for NSW Transport REST API"
 EMAIL = ''
 URL = ""
 KEYWORDS = ["assessment 2",
-    "Trip Planner", "flask restful api"]
+            "Trip Planner", "flask restful api"]
 
 INSTALL_REQUIRES = [
-"astroid",
-"certifi",
-"Click",
-"entrypoints",
-"Flask",
-"isort",
-"itsdangerous",
-"Jinja2",
-"lazy-object-proxy",
-"MarkupSafe",
-"mccabe",
-"pycodestyle",
-"pyflakes"
-"pylint"
-"python-dateutil"
-"six"
-"-e git+https://github.com/joeyabouharb/trip-planner-app.git@f349e18e32c64f5e1c1a57a0b83ec05f8e3c2fc3#egg=trip_planner",
-"typed-ast",
-"urllib3",
-"Werkzeug",
-"wrapt"
+    "astroid",
+    "certifi",
+    "Click",
+    "entrypoints",
+    "Flask",
+    "isort",
+    "itsdangerous",
+    "Jinja2",
+    "lazy-object-proxy",
+    "MarkupSafe",
+    "mccabe",
+    "pycodestyle",
+    "pyflakes"
+    "pylint"
+    "python-dateutil"
+    "six"
+    "git+https://github.com/joeyabouharb/trip-planner-app.git@f349e18e32c64f5e1c1a57a0b83ec05f8e3c2fc3#egg=trip_planner",
+    "typed-ast",
+    "urllib3",
+    "Werkzeug",
+    "wrapt"
 ]
 
 LONG_DESC = """\
